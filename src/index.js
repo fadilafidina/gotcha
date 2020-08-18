b520f0d313be654c9b0ce707ac8dd6414bb3d701
Fadila Fidina <fadila_fidina@hotmail.com>
2020-08-19 00:03:36 +1000

    using cors and env variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
 console.log("Hi sdfsd");
 
 console.log(process.env.MY_SECRET);
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello world hahhhh');
+});
+
+app.get('/example', (req, res) => {
+    res.send('Hello world hahhhh from example');
+});
+
+app.listen(process.env.PORT, () =>
+    console.log(`listening on port ${process.env.PORT}`));
